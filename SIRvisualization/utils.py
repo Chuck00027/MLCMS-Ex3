e25d5c85a68e1f628a1cd7cfbf6f02bcb09e8f0e
Chuck00027 <102159768+Chuck00027@users.noreply.github.com>
2023-05-14 20:29:01 +0200

    Update utils.py

--- SIRvisualization/utils.py
+++ SIRvisualization/utils.py
@@ -6,24 +6,26 @@ import plotly.graph_objects as go
 
 
 def file_df_to_count_df(df,
-                        ID_SUSCEPTIBLE=1,
-                        ID_INFECTED=0):
-    """
+                            ID_SUSCEPTIBLE=1,
+                            ID_INFECTED=0,
+                            ID_RECOVERED=2):
+"""
     Converts the file DataFrame to a group count DataFrame that can be plotted.
-    The ID_SUSCEPTIBLE and ID_INFECTED specify which ids the groups have in the Vadere processor file.
+    The ID_SUSCEPTIBLE and ID_INFECTED specify which ids the groups have in the Vadere processor
+    file.
     """
-    pedestrian_ids = df['pedestrianId'].unique()
-    sim_times = df['simTime'].unique()
-    group_counts = pd.DataFrame(columns=['simTime', 'group-s', 'group-i', 'group-r'])
-    group_counts['simTime'] = sim_times
-    group_counts['group-s'] = 0
-    group_counts['group-i'] = 0
-    group_counts['group-r'] = 0
-
+    pedestrian_ids = df[’pedestrianId’].unique()
+    sim_times = df[’simTime’].unique()
+    group_counts = pd.DataFrame(columns=[’simTime’, ’group-s’, ’group-i’, ’group-r’])
+    group_counts[’simTime’] = sim_times
+    group_counts[’group-s’] = 0
+    group_counts[’group-i’] = 0
+    group_counts[’group-r’] = 0
+                 
     for pid in pedestrian_ids:
-        simtime_group = df[df['pedestrianId'] == pid][['simTime', 'groupId-PID5']].values
+        simtime_group = df[df[’pedestrianId’] == pid][[’simTime’, ’groupId-PID5’]].values
         current_state = ID_SUSCEPTIBLE
-        group_counts.loc[group_counts['simTime'] >= 0, 'group-s'] += 1
+        group_counts.loc[group_counts[’simTime’] >= 0, ’group-s’] += 1
         for (st, g) in simtime_group:
             if g != current_state and g == ID_INFECTED and current_state == ID_SUSCEPTIBLE:
                 current_state = g
@@ -33,7 +35,7 @@ def file_df_to_count_df(df,
                 current_state = g
                 group_counts.loc[group_counts[’simTime’] > st, ’group-i’] -= 1
                 group_counts.loc[group_counts[’simTime’] > st, ’group-r’] += 1
-        break
+                break
     return group_counts
 
 
@@ -45,26 +47,29 @@ def create_folder_data_scatter(folder):
     """
     file_path = os.path.join(folder, "SIRinformation.csv")
     if not os.path.exists(file_path):
-    return None
+        return None
     data = pd.read_csv(file_path, delimiter=" ")
+
     print(data)
+
     ID_SUSCEPTIBLE = 1
     ID_INFECTED = 0
     ID_RECOVERED = 2
+
     group_counts = file_df_to_count_df(data, ID_INFECTED=ID_INFECTED, ID_SUSCEPTIBLE=ID_SUSCEPTIBLE,
     ID_RECOVERED=ID_RECOVERED)
     # group_counts.plot()
     scatter_s = go.Scatter(x=group_counts[’simTime’],
-    y=group_counts[’group-s’],
-    name=’susceptible ’ + os.path.basename(folder),
-    mode=’lines’)
+                           y=group_counts[’group-s’],
+                           name=’susceptible ’ + os.path.basename(folder),
+                           mode=’lines’)
     scatter_i = go.Scatter(x=group_counts[’simTime’],
-    y=group_counts[’group-i’],
-    name=’infected ’ + os.path.basename(folder),
-    mode=’lines’)
+                           y=group_counts[’group-i’],
+                           name=’infected ’ + os.path.basename(folder),
+                           mode=’lines’)
     ### The recovered is same as S, and I
     scatter_r = go.Scatter(x=group_counts[’simTime’],
-    y=group_counts[’group-r’],
-    name=’recovered ’ + os.path.basename(folder),
-    mode=’lines’)
+                           y=group_counts[’group-r’],
+                           name=’recovered ’ + os.path.basename(folder),
+                           mode=’lines’)
     return [scatter_s, scatter_i, scatter_r], group_counts
